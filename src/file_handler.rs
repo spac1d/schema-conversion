9e9e2b8badfe5ae2b812815b3c1c94887d6a0756
Jose Antonio M.H <spacid@protontmail.com>
2020-07-22 01:47:17 +0200

    FileHandler struct now has two fields, output file and json parser struct. FileHandler::new() method updated accordingly to those changes.

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,15 +1,23 @@
+use crate::json_parser::JsonParser;
+
 use std::fs::{File};
 use std::path::Path;
+use std::io::{Lines, BufReader, BufRead};
+use std::fs;
 
 pub struct FileHandler {
-    pub input_file : File,
-    pub output_file: File
+    pub output_file: File,
+    parser: JsonParser,
 }
 
 impl FileHandler{
     pub fn new(input_path: &Path, output_path: &Path) -> std::io::Result<Self> {
-        let input_file = File::open(input_path)?;
+        let input_text = fs::read_to_string(input_path)?;
         let output_file = File::create(output_path)?;
-        Ok(FileHandler{input_file,output_file})
+        let parser = JsonParser{input_text, current_line: 0};
+        Ok(FileHandler{output_file,parser})
+    }
+    pub fn parse(&mut self) -> Result<bool,String>{
+        self.parser.parse()
     }
 }
